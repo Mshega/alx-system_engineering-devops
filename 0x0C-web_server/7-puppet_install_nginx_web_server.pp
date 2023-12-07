fc7189934ee12e2d92601a4ef271d7b8eec271d3
Mshega <viwemrayise99@gmail>
2023-12-07 10:01:53 -0800

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,21 @@
-# Script to install nginx using puppet
-#+    Listens on port 80
-#+    Returns a page containing "Hello World!" when queried
-# A Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements.
+# Setup nginx server
 
-package {'nginx':
+package { 'nginx':
+  ensure     => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+file_line { 'aaaaa':
   ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-  provider => shell,
-
-}
-
-exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
-}
-
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 http://cuberule.com/;\\n\\t}/" /etc/nginx/sites-available/default':
-  provider => shell,
-}
-
-exec {'run':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
