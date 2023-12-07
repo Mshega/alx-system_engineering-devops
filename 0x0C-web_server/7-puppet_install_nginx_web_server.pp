74b673576fd12dfa746d4cc9ce355cc8774b9e27
Mshega <viwemrayise99@gmail>
2023-12-07 09:27:35 -0800

    0x0C. Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,28 @@
+# Script to install nginx using puppet
+#+    Listens on port 80
+#+    Returns a page containing "Hello World!" when queried
+# A Puppet manifest containing commands to automatically configure an Ubuntu machine to respect above requirements.
+
+package {'nginx':
+  ensure => 'present',
+}
+
+exec {'install':
+  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
+  provider => shell,
+
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 http://cuberule.com/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
